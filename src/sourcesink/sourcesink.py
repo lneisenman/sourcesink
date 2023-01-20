b05938b027bfd717c0f82c3cd8ed9a18b1ec2785
Larry Eisenman <lneisenman@hotmail.com>
2023-01-20 17:32:53 -0600

    add CalcA and test

--- src/sourcesink/sourcesink.py
+++ src/sourcesink/sourcesink.py
@@ -9,6 +9,20 @@ def make_trace(timesteps: int, initial: np.ndarray,
     trace = np.zeros((len(initial), timesteps))
     trace[:, 0] = initial
     for i in range(1, timesteps):
-        trace[:, i] = A*trace[:, i-1]
+        trace[:, i] = A@trace[:, i-1]
 
     return trace
+
+
+def calcA(data: np.ndarray) -> np.ndarray:
+    b = np.ravel(data[:, 1:].T)
+    (channels, steps) = data.shape
+    H = np.zeros((channels*(steps-1), channels*channels))
+    for i in range(steps-1):
+        roffset = channels*i
+        for j in range(channels):
+            coffset = channels*j
+            H[roffset+j, coffset:coffset+channels] = data[:, i]
+
+    (X, __, __, __) = np.linalg.lstsq(H, b, rcond=None)
+    return X.reshape((channels, channels))
--- tests/test_sourcesink.py
+++ tests/test_sourcesink.py
@@ -6,5 +6,12 @@ import sourcesink as ss
 
 
 def test_make_trace(trueA):
-    result = np.array([1., 0.9], [1., 1.7])
-    npt.assert_allclose(ss.make_trace(2, [1., 1.], trueA[:2, :2]), result)
+    test = ss.make_trace(2, [1., 1.], trueA[:2, :2])
+    result = np.array([[1., 0.9], [1., 1.7]])
+    npt.assert_allclose(test, result)
+
+
+def test_calcA(trueA):
+    trace = ss.make_trace(10, np.ones(4), trueA)
+    A = ss.calcA(trace)
+    npt.assert_allclose(A, trueA)
