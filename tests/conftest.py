faca0c3b425fffb2a6351d1774b05bb2d0511c43
Larry Eisenman <lneisenman@hotmail.com>
2023-01-20 10:34:34 -0600

    update pyproject and tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import numpy as np
 import pytest
 
 
 @pytest.fixture
-def tester():
-    return 'Tester'
+def trueA():
+    return np.array([0.55, 0.35, 0.65, 0.4], [0.7, 1., 0.9, 0.8],
+                    [0.2, 0.1, 0.45, 0.1], [0.6, 0.8, 0.9, 0.95])
--- tests/test_sourcesink.py
+++ tests/test_sourcesink.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
-import modern_python_template as mpt
+import numpy as np
+import numpy.testing as npt
+import sourcesink as ss
 
 
-def test_template(tester):
-    assert mpt.hello() == 'Hello Larry'
-    assert mpt.hello(tester) == 'Hello Tester'
+def test_make_trace(trueA):
+    result = np.array([1., 0.9], [1., 1.7])
+    npt.assert_allclose(ss.make_trace(2, [1., 1.], trueA[:2, :2]), result)
